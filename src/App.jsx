ccc8ca44b38a8ec3a4afbf020c87bcc121e1c174
AnkitJhadev <jha80250@gmail.com>
2024-06-22 09:04:36 +0530

    Setup routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-
-
-function App() {
-
+import { BrowserRouter ,Route ,Routes } from "react-router-dom"
+import Signin from "./pages/Signin"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+import Contact from "./pages/Contact"
+export default function App() {
   return (
-    <>
-      <div>
-        <h1>Hello There</h1>
-      </div>
+   <BrowserRouter>
+     
+     <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/about" element = {<About/>} />
+      <Route path="/contact" element = {<Contact/>} />
+      <Route path="/signin" element = {<Signin/>} />
+      <Route path="/signup" element = {<Signup/>} />
+      <Route path="/profile" element = {<Profile/>} />
 
-    </>
+
+
+
+
+     </Routes>
+
+   </BrowserRouter>
   )
 }
-
-export default App
