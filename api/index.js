069d2c0bb998d19ea1755d74862833d602e26cf3
AnkitJhadev <jha80250@gmail.com>
2024-06-23 13:27:23 +0530

    "coonected to moongose"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
+
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const port = 3000 ;
+const port = 8001;
 
+// Connect to MongoDB
+mongoose.connect( "mongodb+srv://ankitjha:ankitjha@cluster0.e3dwc7p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+  console.log('MongoDB Connected');
+})
+.catch((err) => {
+  console.error('MongoDB Connection Error:', err);
+  process.exit(1); // Exit process with failure
+});
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
